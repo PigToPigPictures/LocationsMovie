0e3bc04bf77e40d121534b4c0ac4a55d70a87f0c
PigToPigPictures <PigtoPigPictures@gmail.com>
2025-01-17 20:52:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// JavaScript goes here
+// Log a message to ensure the script is connected
+console.log("Welcome to Locations Movie!");
+
+// Example: Display an alert when the page loads
+window.addEventListener("load", () => {
+  alert("Welcome to the Locations Movie website!");
+});
