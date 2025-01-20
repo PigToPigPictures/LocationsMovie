2be300587eb0b2d1f2ddb1a455f7d7fc55bdfd62
PigToPigPictures <PigtoPigPictures@gmail.com>
2025-01-20 15:50:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Log a message to ensure the script is connected
-console.log("Locations");
 
-// Example: Display an alert when the page loads
-window.addEventListener("load", () => {
-  alert("Locations");
-});
